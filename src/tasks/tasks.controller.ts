1b854276edf45a160a25d2fb0030c4f9f97dff1b
Yudi Andela <yudhi.andhela@gmail.com>
2019-11-20 20:56:16 +0700

    Update Status Task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -16,6 +16,7 @@ import { CreateTaskDto } from './dto/createTask.dto';
 import { TasksService } from './tasks.service';
 import { TaskStatusValidationPipe } from './pipes/taskStatusValidation.pipe';
 import { Task } from './task.entity';
+import { TaskStatus } from './taskStatus.enum';
 
 @Controller('tasks')
 export class TasksController {
@@ -46,11 +47,11 @@ export class TasksController {
     this.tasksService.deleteTask(id);
   }
 
-  // @Patch('/:id/status')
-  // updateTaskStatus(
-  //   @Param('id') id: string,
-  //   @Body('status', TaskStatusValidationPipe) status: TaskStatus,
-  // ): Task {
-  //   return this.tasksService.updateTaskStatus(id, status);
-  // }
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+  ): Promise<Task> {
+    return this.tasksService.updateTaskStatus(id, status);
+  }
 }
