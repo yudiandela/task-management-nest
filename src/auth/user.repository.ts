fac922ccd4b9f4705abd2365cf2f16a30edd6826
Yudi Andela <yudhi.andhela@gmail.com>
2019-11-21 10:45:33 +0700

    Error Handling

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,10 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/authCredentials.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -11,8 +15,15 @@ export class UserRepository extends Repository<User> {
     user.username = username;
     user.password = password;
 
-    await user.save();
-
-    return user;
+    try {
+      await user.save();
+      return user;
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists!');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
